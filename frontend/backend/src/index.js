a6e27cda73e18ab7bfc0565c563e073f817d0a40
nikcodes69 <nikhilthapa065@gmail.com>
2025-06-15 10:20:38 +0545

    frontend setup & Add LoginPage

---
+++ frontend/backend/src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import connectDB from './lib/db.js';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+    connectDB();
+})
---
+++ frontend/backend/src/lib/utils.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (userId,res)=>{
+
+    const token = jwt.sign({userId},process.env.JWT_SECRET,{
+        expiresIn: '1h'
+    
+    })
+
+    res.cookie("jwt",token,{
+        maxAge: 60*60*1000, // 1 hour
+        httpOnly: true, //prevent client side js from accessing the cookie(XSS attacks)
+        sameSite: 'strict', // Prevent CSRF attacks
+        secure: process.env.NODE_ENV !== 'development'
+    });
+
+    return token;
+}
