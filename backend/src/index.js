29c3778e6dc846caf98315039849017482417f76
nikcodes69 <nikhilthapa065@gmail.com>
2025-06-17 22:59:16 +0545

    added cors, msg & auth routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,11 @@ import cookieParser from 'cookie-parser';
 
 import connectDB from './lib/db.js';
 
+import cors from "cors";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js"; 
+
 dotenv.config();
 
 const app = express();
@@ -12,6 +17,15 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+);
+
+app.use('/api/auth',authRoutes);
+app.use('/api/message',messageRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
